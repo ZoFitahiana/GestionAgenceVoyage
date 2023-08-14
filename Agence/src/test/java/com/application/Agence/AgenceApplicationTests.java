c12f5b4d706d90cbcec20520dacd97dd50b4d3a0
Fitahi <hei.tsiaroniaina@gmail.com>
2023-08-14 22:46:38 +0300

    feat : Create application spring boot

---
+++ Agence/src/test/java/com/application/Agence/AgenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.Agence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
