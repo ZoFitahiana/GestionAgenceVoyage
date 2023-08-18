f3f942904f3dd7f6316454b5db5bbe038c39dd21
Fitahi <hei.tsiaroniaina@gmail.com>
2023-08-18 16:04:36 +0300

    feat:Create client service

---
+++ Agence/src/main/java/com/application/Agence/Service/ClientService.java
@@ -0,0 +1,34 @@
+package com.application.Agence.Service;
+
+import com.application.Agence.Model.Client;
+import com.application.Agence.Repository.ClientDAO;
+import lombok.Data;
+import org.springframework.stereotype.Service;
+import java.util.List;
+@Data
+@Service
+public class ClientService {
+    private ClientDAO clients ;
+    public ClientService(ClientDAO clients) {
+        this.clients = clients;
+    }
+    public Client FindByIdClient(int id){
+        return clients.FindById(id);
+    }
+
+    public List<Client> FindAllClient(){
+       return clients.FindAll();
+    }
+    public Client AddClient(Client entity){
+        return clients.Register(entity);
+    }
+    public Client UpdateClient(int id,Client client){
+        return clients.Update(id ,client);
+    }
+    public Client UpdateColumnClient(int id , Client client){
+        return clients.UpdatePartial(id , client);
+    }
+    public  List<Client> DeleteClient(int id ){
+        return clients.Delete(id);
+    }
+}
