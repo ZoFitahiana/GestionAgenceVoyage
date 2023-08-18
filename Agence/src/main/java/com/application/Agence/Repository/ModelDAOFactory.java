fbb4091bc02e3e44658ffb21d3d60fc2799e11aa
Fitahi <hei.tsiaroniaina@gmail.com>
2023-08-18 16:03:50 +0300

    feat : Create model DAO factory

---
+++ Agence/src/main/java/com/application/Agence/Repository/ModelDAOFactory.java
@@ -0,0 +1,13 @@
+package com.application.Agence.Repository;
+
+import java.util.List;
+
+public interface ModelDAOFactory<T>{
+    public T FindById(int id_client);
+    public List<T> FindAll();
+    public T Register(T entity);
+    public T Update(int id, T entity);
+    public T UpdatePartial(int id, T entity);
+    public List<T> Delete(int id);
+
+}
