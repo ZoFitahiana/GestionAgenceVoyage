5b492ba33e27eff687a1356640a8a2fb62965ea9
Fitahi <hei.tsiaroniaina@gmail.com>
2023-08-24 10:46:37 +0300

    feat : create Test interface factory

---
+++ Agence/src/test/java/com/application/Agence/InterfaceTestFactory.java
@@ -0,0 +1,11 @@
+package com.application.Agence;
+
+import org.junit.jupiter.api.Test;
+
+public interface InterfaceTestFactory {
+    public void TestFindById();
+    public void TestFindAll();
+    public void TestPost();
+    public void TestUpdate();
+    public void TestDelete();
+}
